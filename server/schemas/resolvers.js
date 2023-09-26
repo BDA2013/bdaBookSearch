075e642bcc4b87e3dbbd68b9b8d575acd9deb6d1
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-09-26 05:11:36 -0400

    Imported ApolloServer and Schemas

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,12 @@
+const { User, Thought } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+  },
+
+  Mutation: {
+  }
+};
+
+module.exports = resolvers;
